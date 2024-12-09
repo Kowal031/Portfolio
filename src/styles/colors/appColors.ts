0a576ffe98de7046f7cbd5f8b68e173eb09dc92a
Maciej <maciej.0.kowalczyk@euvic.pl>
2024-12-09 22:10:30 +0100

    Added env template, images basic colors and installed package

---
+++ src/styles/colors/appColors.ts
@@ -0,0 +1,35 @@
+export const appColors = {
+  primary: {
+    main: "#1a2a3a",
+  },
+  secondary: {
+    main: "#4B6A84",
+  },
+  background: {
+    default: "#05101C",
+    paper: "#293642",
+  },
+  text: {
+    primary: "#f0f0f0",
+    secondary: "#f5f2e9",
+    tertiary: "#b0b0b0",
+  },
+  action: {
+    hover: "#c2b280",
+    active: "#cab678",
+  },
+  inputBorder: {
+    error: "#FF0000",
+    white: "#ffffff",
+  },
+
+  custom: {
+    star: "#ffffff",
+    elementsBackground: "#ffffff1a",
+    btnHover: "#14212E",
+    gloomyPurple: "#854CE6",
+    persianIndigo: "#371079",
+    white: "#ffffff",
+    menuButton: "#cab678",
+  },
+};
