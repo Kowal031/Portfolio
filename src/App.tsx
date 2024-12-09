00be0ca16a60be65ba4ee467b938fe6fb8fa2be0
Maciej <maciej.0.kowalczyk@euvic.pl>
2024-12-09 23:07:48 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import { FC } from "react";
+import Layout from "./components/layout/Layout";
+import StarsBackground from "./components/starsBackground/StarsBackground";
+import AboutSection from "./sections/aboutSection/AboutSection";
+import ContactSection from "./sections/contactSection/ContactSection";
+import ExperienceSection from "./sections/experienceSection/ExperienceSection";
+import HeroSection from "./sections/heroSection/HeroSection";
+import ProjectsSection from "./sections/projectsSection/ProjectsSection";
+import SkillsSection from "./sections/skillsSection/SkillsSection";
 
 const App: FC = () => {
-  return <>Portfolio Maciej Kowalczyk</>;
+  return (
+    <Layout>
+      <StarsBackground />
+      <HeroSection />
+      <AboutSection />
+      <ExperienceSection />
+      <ProjectsSection />
+      <SkillsSection />
+      <ContactSection />
+    </Layout>
+  );
 };
 
 export default App;
