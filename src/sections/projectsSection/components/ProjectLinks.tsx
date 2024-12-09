94bfe27ebfea4a6e0950f83bd8b0cccbc3e26b53
Maciej <maciej.0.kowalczyk@euvic.pl>
2024-12-10 00:06:22 +0100

    Added cv file and changed projects links

--- src/sections/projectsSection/components/ProjectLinks.tsx
+++ src/sections/projectsSection/components/ProjectLinks.tsx
@@ -14,13 +14,17 @@ interface ProjectLinksProps {
 const ProjectLinks: FC<ProjectLinksProps> = ({ githubUrl, liveUrl }) => {
   return (
     <ButtonWrapper>
-      <StyledGithubButton variant="contained" href={githubUrl}>
-        {PROJECTS_CONSTANTS.BUTTONS_LABELS.GITHUB}
-      </StyledGithubButton>
+      <a href={githubUrl} target="_blank">
+        <StyledGithubButton variant="contained">
+          {PROJECTS_CONSTANTS.BUTTONS_LABELS.GITHUB}
+        </StyledGithubButton>
+      </a>
       {liveUrl && (
-        <StyledLiveDemoButton variant="outlined" href={liveUrl}>
-          {PROJECTS_CONSTANTS.BUTTONS_LABELS.LIVE_DEMO}
-        </StyledLiveDemoButton>
+        <a href={githubUrl} target="_blank">
+          <StyledLiveDemoButton variant="outlined" href={liveUrl}>
+            {PROJECTS_CONSTANTS.BUTTONS_LABELS.LIVE_DEMO}
+          </StyledLiveDemoButton>
+        </a>
       )}
     </ButtonWrapper>
   );
